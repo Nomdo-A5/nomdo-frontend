3bea526969b74170867e2ba84fcba6254597382a
Rahmafatin Nurul Izza <rhmftn.ni@gmail.com>
2021-11-17 16:29:12 +0700

    Fix logic on creating new balance

--- src/components/floatingMoneyReport/FloatingMoneyReport.js
+++ src/components/floatingMoneyReport/FloatingMoneyReport.js
@@ -5,92 +5,184 @@ import Draggable from 'react-draggable';
 import { BsWallet2 } from 'react-icons/bs';
 import { useState, useContext } from 'react';
 
-import { Card, Row, Col, Input, Select } from 'antd';
+import { Form, Card, Row, Col, Input, Select } from 'antd';
 import { ClockCircleOutlined, MailOutlined, UserOutlined } from '@ant-design/icons'
 import { BsPencilSquare } from 'react-icons/bs';
 import { GrAddCircle } from 'react-icons/gr';
 import '../floatingMoneyReport/FloatingMoneyReport.css';
 import { WorkspaceContext, WorkspaceContextProvider } from '../../context/WorkspaceContext';
+import { getToken } from '../../utils/authentication';
+import axios from 'axios';
+import { BASE_API_URL } from '../../constants/urls';
 
-export const FloatingMoneyReport = () => {
-    
-    const { Option } = Select;
+const NewBalanceForm = ({ visible, onCreate, onCancel }) => {
+    const [form] = Form.useForm();
     const context = useContext(WorkspaceContext)
-    const [isModalVisible, setIsModalVisible] = useState(false);
+    const { Option } = Select;
+    return (
+        <Modal
+            title="New Balance"
+            visible={visible}
+            onOk={() => {
+                form
+                    .validateFields()
+                    .then((values) => {
+                        form.resetFields();
+                        onCreate(values);
+                    })
+                    .catch((info) => {
+                        console.log('Validate Failed:', info);
+                    });
+            }}
+            onCancel={onCancel}
+            style={{ textAlign: "center" }}
+            okText="Add"
+            width={340}>
 
-    const showModal = () => {
-        setIsModalVisible(true);
-    };
+            <Form
+                form={form}
+                layout="vertical"
+                name="form_in_modal"
 
-    const handleOk = () => {
-        setIsModalVisible(false);
-    };
+            >
+                <div>
+                    <Row>
+                        <div className="workspace-name-and-input">
+                            <div className="input-area-drop-down">
+                                <Form.Item
+                                    name="workspace_id"
+                                    label="Workspace name"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please select workspace!',
+                                        },
+                                    ]}
 
-    const handleCancel = () => {
-        setIsModalVisible(false);
+                                >
+                                    <Select
+                                        style={{ width: 270 }}
+                                        placeholder="select your workspace"
+                                    >
+                                        {context.workspace.map(w =>
+                                            (<Option value={w.id}>{w.workspace_name}</Option>)
+                                        )}
+                                    </Select>
+
+                                </Form.Item>
+                            </div>
+                        </div>
+                    </Row>
+                    <Row>
+                        <Form.Item
+                            name="nominal"
+                            label="Nominal"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input the new task name!',
+                                },
+                            ]}
+                        >
+                            <div className="nominal-name-and-input">
+                                <div className="form-input-nominal-name">
+                                    <Input placeholder="Nominal" style={{ borderRadius: "10px 10px 10px 10px" }} />
+                                </div>
+                            </div>
+                        </Form.Item>
+
+                    </Row>
+                    <Row>
+                        <div className="tipe-name-and-input">
+                            <div className="input-area-drop-down">
+                                <Form.Item
+                                    name="is_income"
+                                    label="Type"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please select type!',
+                                        },
+                                    ]}
+
+                                >
+                                    <Select
+                                        style={{ width: 270 }}
+                                        placeholder="select type"
+                                        >
+                                        
+                                            <Option value='1'>income</Option>
+                                            <Option value='0'>outcome</Option>
+                                        
+                                    </Select>
+
+                                </Form.Item>
+                            </div>
+                        </div>
+
+
+                    </Row>
+                    <Row>
+                        <Form.Item
+                            name="balance_description"
+                            label="Desciption"
+                            className="new-balance-form_last-form-item"
+                        >
+                            <div className="description-name-and-input">
+
+                                <div className="form-input-description-name">
+                                    <Input.TextArea placeholder="Deskripsi" style={{ borderRadius: "10px 10px 10px 10px" }} />
+                                </div>
+                            </div>
+                        </Form.Item>
+
+                    </Row>
+                </div>
+            </Form>
+
+        </Modal>
+    )
+}
+export const FloatingMoneyReport = () => {
+
+
+    const [visible, setVisible] = useState(false);
+
+    const onCreate = async (values) => {
+        const token = getToken();
+        const response = await axios.post(BASE_API_URL + 'balance', {
+            workspace_id: values.workspace_id,
+            nominal: values.nominal,
+            is_income: values.is_income,
+            balance_description: values.balance_description
+        },
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+            });
+        console.log(response)
+        setVisible(false);
     };
 
     return (
-        <>
-          <Button type="primary" onClick={showModal}>
-            New Balance
-          </Button>
-          <Modal title="New Balance" okText="Add"
-            visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}
-            style={{ textAlign: "center" }} 
-            width={340}>
-            
-            <div>
-                <Row>
-                    <div className="workspace-name-and-input">
-                        <div className="workspace-name">
-                            Workspace
-                        </div>
-                        <div className="input-area-drop-down">
-                            <Select style={{ width: 270 }} placeholder="Select your workspace">
-                            {context.workspace.map(w =>
-                                    (<Option value={w.id}>{w.workspace_name}</Option>)
-                            )}
-                            </Select>
-                        </div>
-                    </div>
-                </Row>
-                <Row>
-                    <div className="nominal-name-and-input">
-                        <div className="nominal-name">
-                            Nominal
-                        </div>
-                        <div className="form-input-nominal-name">
-                            <Input placeholder="Nominal"  style={{ borderRadius:"10px 10px 10px 10px"}}/>
-                        </div>
-                    </div>
-                </Row>
-                <Row>
-                    <div className="tipe-name-and-input">
-                        <div className="tipe-name">
-                            Tipe
-                        </div>
-                        <div className="input-area-drop-down">
-                            <Select style={{ width: 270 }} placeholder="Select your workspace">
-                            {context.workspace.map(w =>
-                                    (<Option value={w.id}>{w.workspace_name}</Option>)
-                            )}
-                            </Select>
-                        </div>
-                    </div>
-                </Row>
-                <Row>
-                    <div className="description-name-and-input">
-                        <div className="description-name">
-                            Deskripsi
-                        </div>
-                        <div className="form-input-description-name">
-                            <Input placeholder="Deskripsi"  style={{ borderRadius:"10px 10px 10px 10px"}}/>
-                        </div>
-                    </div>
-                </Row>
-            </div>
-          </Modal>
-        </>
-      );
-    }
+        <div>
+            <Button
+                type="primary"
+                onClick={() => {
+                    setVisible(true);
+                }}
+            >
+                New Balance
+
+            </Button>
+            <NewBalanceForm
+                visible={visible}
+                onCreate={onCreate}
+                onCancel={() => {
+                    setVisible(false);
+                }}
+            />
+        </div>
+    );
+}
