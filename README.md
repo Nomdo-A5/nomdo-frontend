d7c913b0edf8d699e323a5ac9882982176232bdc
Paulus Bimo <kingofbimo@gmail.com>
2021-11-23 11:21:20 +0700

    New Board UI Fixes #75

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 # Aplikasi kelompok A5 - Nomdo
 
-# Sprint 1 - Revisi 01
+# Sprint 2
 
 # Project ini telah menggunakan:
 
 1. Autentikasi Login Username dan Password
 2. CRUD Workspace pada halaman Dashboard
 3. CRUD Board pada halaman Board
-4. Logout
+4. Menampilkan report pada workspace
 
 *Data session dan CRUD disimpan dalam Local Storage.
 
-# Beberapa Framework dan Plugin yang digunakan:
+# Beberapa Framework, Plugin, dan Material Design yang digunakan:
 
-1. React Bootstrap
+1. React Bootstrap (bootstrap@5.1.3 or Newer)
 2. React Icons
-3. Sweet Alert
+3. Ant Design
+
+# Server
+
+Zerotier ID: 9f77fc393e420b3f
+*Kalau website tidak me-load data, berarti server off. Chat saja kelompok A5
--- src/components/progressBar/ProgressBar.js
+++ src/components/progressBar/ProgressBar.js
@@ -6,28 +6,10 @@ import ProgressBarPercent from 'react-bootstrap/ProgressBar'
 
 export default function ProgressBar (props) {
     return (
-        <div>
-            <Card
-                style={{
-                    alignContent: "center",
-                    width: "750px",
-                    height: "auto",
-                    borderRadius: "16px",
-                    margin: "0 auto",
-                    boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.6)",
-                    backgroundColor: "#F6F6F6"
-                }}
-                
-            >
-                <div className="progress-bar-form">
-                    <div className="progress-bar-title">
-                        {props.board_name}
-                    </div>
-                    <div className="progress-bar-chart">
-                        <ProgressBarPercent variant="success" now={80} />
-                    </div>
-                </div>
-            </Card>
+        <div className="progress-bar-form">
+            <div className="progress-bar-chart">
+                <ProgressBarPercent borderRadius="50px" variant="success" now={80} />
+            </div>
         </div>
     );
 }
