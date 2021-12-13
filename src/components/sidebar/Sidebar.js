8ed60c0fbe569ed5f664a9d5d021b78b2b68c497
Rahmafatin Nurul Izza <rhmftn.ni@gmail.com>
2021-12-13 11:48:06 +0700

    Delete workspace

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Layout, Menu, Avatar } from 'antd';
+import { Layout, Menu, Avatar, Modal } from 'antd';
 import { NotificationOutlined } from '@ant-design/icons';
 import { BASE_API_URL } from "../../constants/urls";
 import axios from "axios";
 import { getToken, isLogin } from "../../utils/authentication";
 import { BrowserRouter as Router, Route, Link, useHistory } from "react-router-dom";
 import { WorkspaceContext } from "../../context/WorkspaceContext";
+import { ExclamationCircleOutlined } from '@ant-design/icons';
 
 import "./Sidebar.css";
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
+
 // function refreshPage() {
 //   window.location.reload(true);
 // }
@@ -21,25 +23,56 @@ const { Header, Content, Sider } = Layout;
 
 const Sidebar = () => {
   const history = useHistory();
-
+  const { confirm } = Modal;
   const context = useContext(WorkspaceContext)
+  const token = getToken()
 
-  function handleBoardClicked(id){
-    history.push("/board/"+id)
+  function handleBoardClicked(id) {
+    history.push("/board/" + id)
     //onClick={() => history.push("/workspace")}
     // onClick={() => history.push("/dashboard")}
     // onClick={() => handleBoardClicked(w.id)}
     // onClick={() => handleReportClicked(w.id) } 
   }
 
-  function handleReportClicked(id){
-    history.push("/report/"+id)
+  function handleReportClicked(id) {
+    history.push("/report/" + id)
     window.location.reload()
   }
-  function handleHomeClicked(){
+  function handleHomeClicked() {
     history.push("/home")
     window.location.reload()
   }
+
+  function showDeleteConfirm($id) {
+    confirm({
+      title: 'Are you sure delete this balance?',
+      icon: <ExclamationCircleOutlined />,
+      content: 'Some descriptions',
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
+      onOk() {
+        deleteWorkspace($id)
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  }
+  const deleteWorkspace = async ($id) => {
+    console.log("TOKENN " + token)
+    const response = await axios.delete(BASE_API_URL + 'workspace', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        data: {
+            id: $id
+        }
+    });
+
+    console.log(response)
+}
   return (
     <Router>
       <Sider width={200} className="site-layout-background" style={{
@@ -62,25 +95,30 @@ const Sidebar = () => {
           </Menu.Item>
           <Menu.Item key="workspace">
             <span>Workspace</span>
-            <Link to="/workspace"  />
+            <Link to="/workspace" />
           </Menu.Item>
-          {context.workspace.map(w => (            
-            <SubMenu key={w.id} title={w.workspace_name} icon={<Avatar>{w.workspace_name.charAt(0).toUpperCase()}</Avatar>}>              
-              <Menu.Item key={"dahboard "+w.id}>
+          {context.workspace.map(w => (
+            <SubMenu key={w.id} title={w.workspace_name} icon={<Avatar>{w.workspace_name.charAt(0).toUpperCase()}</Avatar>}>
+              <Menu.Item key={"dahboard " + w.id}>
                 <span>Dashboard</span>
-                <Link to={{ pathname: `/workspace/${w.id}/dashboards`, state: { workspace: w.id } }}  />
+                <Link to={{ pathname: `/workspace/${w.id}/dashboards`, state: { workspace: w.id } }} />
               </Menu.Item>
-              <Menu.Item key={"board "+w.id}>
+              <Menu.Item key={"board " + w.id}>
                 <span>Board</span>
                 <Link to={{ pathname: `/workspace/${w.id}/boards`, state: { workspace: w.id } }} />
               </Menu.Item>
-              <Menu.Item key={"report "+w.id} >
+              <Menu.Item key={"report " + w.id} >
                 <span>Money Report</span>
                 <Link to={{ pathname: `/report/${w.id}`, state: { workspace: w.id } }} />
               </Menu.Item>
               <Menu.Item >
                 Member
-                <Link to="/member"/>
+                <Link to="/member" />
+              </Menu.Item>
+              <Menu.Item onClick={() => {
+                showDeleteConfirm(w.id)
+              }}>
+                <span style={{ color: 'red' }}>Delete Workspace</span>
               </Menu.Item>
             </SubMenu>
           ))}
